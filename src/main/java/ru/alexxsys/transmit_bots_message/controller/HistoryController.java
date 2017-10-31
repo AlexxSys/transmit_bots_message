87d7741566415f2fcdee2910f4be3ad1a0c6a52c
AlexxSys <AlexxSys@GMail.com>
2017-11-01 00:38:44 +0300

    Изменил ответ по show-history на REST

--- src/main/java/ru/alexxsys/transmit_bots_message/controller/HistoryController.java
+++ src/main/java/ru/alexxsys/transmit_bots_message/controller/HistoryController.java
@@ -5,21 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ru.alexxsys.transmit_bots_message.entity.History;
 import ru.alexxsys.transmit_bots_message.repository.HistoryRepository;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class HistoryController {
-    private final HistoryRepository repository;
+    private final HistoryRepository repositoryHistory;
 
     @Autowired
-    private HistoryController(HistoryRepository repository) {
-        this.repository = repository;
+    private HistoryController(HistoryRepository repositoryHistory) {
+        this.repositoryHistory = repositoryHistory;
     }
 
     @RequestMapping(path = "/show-jurnal", method = RequestMethod.GET)
-    public String showHistory(Model model){
+    public List<History> showHistory(Model model){
 //                              @RequestParam Map<String, String> parameters){
 
-        return "history";
+        return repositoryHistory.findAll();
     }
 }
